81498d89194080c4eb0e9450f996b5bbf3bfd279
Pratham Sawant <codevps07@gmail.com>
2022-07-25 21:48:34 +0530

    doing working

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -6,37 +6,37 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng,
-} from "react-places-autocomplete";
+// import React, { useState } from "react";
+// import PlacesAutocomplete, {
+//   geocodeByAddress,
+//   getLatLng,
+// } from "react-places-autocomplete";
 import useStyles from "./styles";
 import "./styles.css";
 
 const Hero = () => {
   const classes = useStyles();
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  const sp = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    });
-    console.log(latitude, longitude);
-  };
-  const [address, setAddress] = useState("");
-  const [coordinates, setCoordinates] = useState({
-    lat: null,
-    lng: null,
-  });
+  // const [latitude, setLatitude] = useState(0);
+  // const [longitude, setLongitude] = useState(0);
+  // const sp = () => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     setLatitude(position.coords.latitude);
+  //     setLongitude(position.coords.longitude);
+  //   });
+  //   console.log(latitude, longitude);
+  // };
+  // const [address, setAddress] = useState("");
+  // const [coordinates, setCoordinates] = useState({
+  //   lat: null,
+  //   lng: null,
+  // });
 
-  const handleSelect = async (value) => {
-    const results = await geocodeByAddress(value);
-    const latLng = await getLatLng(results[0]);
-    setAddress(value);
-    setCoordinates(latLng);
-  };
+  // const handleSelect = async (value) => {
+  //   const results = await geocodeByAddress(value);
+  //   const latLng = await getLatLng(results[0]);
+  //   setAddress(value);
+  //   setCoordinates(latLng);
+  // };
   // first find latlng of place typed and find restaurants in 3-4km of the radius
   return (
     <div>
@@ -47,7 +47,7 @@ const Hero = () => {
         <Typography variant="h5" className={classes.heading}>
           <i> safe & fast food delivery with great discounts</i>
         </Typography>
-        <PlacesAutocomplete
+        {/* <PlacesAutocomplete
           value={address}
           onChange={setAddress}
           onSelect={handleSelect}
@@ -66,12 +66,10 @@ const Hero = () => {
 
               <div>
                 {loading ? <div>...loading</div> : null}
-
                 {suggestions.map((suggestion) => {
                   const style = {
                     backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
                   };
-
                   return (
                     <div {...getSuggestionItemProps(suggestion, { style })}>
                       {suggestion.description}
@@ -81,21 +79,23 @@ const Hero = () => {
               </div>
             </div>
           )}
-        </PlacesAutocomplete>
+        </PlacesAutocomplete> */}
         <TextField
           className={classes.search}
           name="search"
           variant="outlined"
-          label={`Search for Restaurants`}
+          label={`Geolocation coming soon`}
+          disabled="true"
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
                 <IconButton>
-                  <SearchIcon />
+                  <SearchIcon disabled="true" />
                 </IconButton>
                 <IconButton>
                   <MyLocationIcon
-                    onClick={() => sp()}
+                    disabled="true"
+                    // onClick={() => sp()}
                     style={{ color: "coral" }}
                   />
                 </IconButton>
